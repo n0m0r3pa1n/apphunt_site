8499624c150b057e422e7c7f173822c671e07575
Polina Zhelyazkova <poly_vjk@abv.bg>
2015-04-30 17:19:30 +0300

    Replace social icons

--- src/app/components/AboutUs.jsx
+++ src/app/components/AboutUs.jsx
@@ -18,21 +18,24 @@ var AboutUs = React.createClass({
             boxShadow: '0 1px 1px rgba(0, 0, 0, 0.12);'
         }
 
-        var imgStyle = {
-            width: 40,
-            height: 40,
-            marginTop: 10,
+        var iconStyle = {
+            color: "#FFF",
+            fontSize: 35,
             marginLeft: 10,
             marginRight: 10,
             marginBottom: 20
         }
+        var iconContainerStyle = {
+            display: "inline-block",
+            marginTop: 10
+        }
         return (
             <div className="pull-right">
-                <a href="https://twitter.com/TheAppHunt" target="_blank"><img src="./res/img/twitter.png" style={imgStyle} /></a>
-                <a href="https://www.facebook.com/theapphunt" target="_blank"><img src="./res/img/facebook.png" style={imgStyle} /></a>
-                <a href="http://theapphunt.tumblr.com/" target="_blank"><img src="./res/img/tumblr.png" style={imgStyle} /></a>
-                <a href="https://apphunters.wordpress.com/" target="_blank"><img src="./res/img/wordpress.png" style={imgStyle} /></a>
-                <a href="https://www.youtube.com/channel/UCPnLaWyjNC6feTXUuhggJ-w" target="_blank"><img src="./res/img/youtube.png" style={imgStyle} /></a>
+                <a href="https://twitter.com/TheAppHunt" target="_blank" className="social_icon_cont"><span className="sfmsb-icon-twitter sfmsb-circle" style={iconStyle}></span></a>
+                <a href="https://www.facebook.com/theapphunt" target="_blank" className="social_icon_cont"><span className="sfmsb-icon-facebook sfmsb-circle" style={iconStyle}></span></a>
+                <a href="http://theapphunt.tumblr.com/" target="_blank" className="social_icon_cont"><span className="sfmsb-icon-tumblr sfmsb-circle" style={iconStyle}></span></a>
+                <a href="https://apphunters.wordpress.com/" target="_blank" className="social_icon_cont"><span className="sfmsb-icon-wordpress sfmsb-circle" style={iconStyle}></span></a>
+                <a href="https://www.youtube.com/channel/UCPnLaWyjNC6feTXUuhggJ-w" target="_blank" className="social_icon_cont"><span className="sfmsb-icon-youtube sfmsb-circle" style={iconStyle}></span></a>
             </div>
         )
     },
